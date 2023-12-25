9cadda92b9007905914c69f5117c055cb539797e
portellii <119754985+alecdportelli@users.noreply.github.com>
2023-12-24 20:10:43 -0500

    Added responsiveness to home page

--- portfolio2024/src/components/footer.js
+++ portfolio2024/src/components/footer.js
@@ -5,17 +5,28 @@ const Footer = () => {
   return (
     <footer>
         <div className="footer-container">
+
             <div className="left-column">
                 <div className="left-top">Let's start building.</div>
                 <div className="left-bottom">
                     <button className='connectButton'>Connect</button>
                 </div>
             </div>
+
             <div className="right-column">
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
                     <button className='footerButton hover-underline-animation '>Resume</button>
+                </a>
+
+                <a href="https://github.com/alecdportelli" target="_blank" rel="noopener noreferrer">
                     <button className='footerButton hover-underline-animation '>GitHub</button>
+                </a>
+
+                <a href="https://www.linkedin.com/in/alec-portelli/" target="_blank" rel="noopener noreferrer">
                     <button className='footerButton hover-underline-animation '>LinkedIn</button>
+                </a>
             </div>
+
         </div>
 
         <div>
